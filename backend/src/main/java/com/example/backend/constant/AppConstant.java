1626efbc0555c006e34839c2bb240708ad82a9af
nguyenhieu31 <hieufaptv777@gmail.com>
2025-01-12 11:25:41 +0700

    fix error 'hh-mm-ss' to 'yyyy-mm-dd'

--- backend/src/main/java/com/example/backend/constant/AppConstant.java
+++ backend/src/main/java/com/example/backend/constant/AppConstant.java
@@ -3,5 +3,5 @@ package com.example.backend.constant;
 public class AppConstant {
     public static final int CODE_SUCCESS = 200;
     public static final String MESSAGE_SUCCESS = "Success";
-    public static final String TIME_FORMAT = "HH:mm:ss";
+    public static final String TIME_FORMAT = "yyyy-mm-dd";
 }

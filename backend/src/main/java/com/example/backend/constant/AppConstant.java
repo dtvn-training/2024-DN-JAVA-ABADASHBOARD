cace4f20a963243b6c3613c7172b62eed637c4e9
nguyenhieu31 <hieunguyenvan3110@gmail.com>
2025-01-14 13:13:12 +0700

    fix file AppConstant.java

--- backend/src/main/java/com/example/backend/constant/AppConstant.java
+++ backend/src/main/java/com/example/backend/constant/AppConstant.java
@@ -3,5 +3,5 @@ package com.example.backend.constant;
 public class AppConstant {
     public static final int CODE_SUCCESS = 200;
     public static final String MESSAGE_SUCCESS = "Success";
-    public static final String TIME_FORMAT = "yyyy-mm-dd";
+    public static final String TIME_FORMAT = "YYYY-MM-DD";
 }
